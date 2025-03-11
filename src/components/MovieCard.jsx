1f8715e65ace05d40f55f3a213414a7657a4e3ae
nicolo_canevari <nico.canevari@outlook.it>
2025-03-11 17:27:38 +0100

    [FEAT]creo il componente MovieCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,33 @@
+// COMPONENTE PER VISUALIZZARE IL SINGOLO FILM
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Componente MovieCard che rappresenta una singola card di film
+const MovieCard = ({ movie }) => {
+
+    return (
+
+        <div className="movie-card">
+
+            {/* Immagine della locandina del film */}
+            <img src={movie.image} alt={movie.title} />
+
+            {/* Testo della Card del film */}
+            <div className="details">
+
+                <h2>{movie.title}</h2>
+                <p><strong>Regista:</strong> {movie.director}</p>
+                <p><strong>Trama:</strong> {movie.abstract}</p>
+
+                {/* Bottone per la navigazione alla pagina MovieDetail */}
+                <Link to={`/movie/${movie.id}`}>Vai ai Dettagli</Link>
+
+            </div>
+
+        </div>
+
+    );
+
+};
+
+export default MovieCard;
