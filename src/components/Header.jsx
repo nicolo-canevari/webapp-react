dd0e579f4b0db2c12ea88693fa6e8312c2528176
nicolo_canevari <nico.canevari@outlook.it>
2025-03-11 17:14:34 +0100

    [FEAT] header senza navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+
+        <header>
+
+            <h1>Libreria dei miei Film</h1>
+
+        </header>
+
+    );
+
+};
+
+export default Header;
